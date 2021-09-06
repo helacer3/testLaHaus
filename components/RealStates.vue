07b43211ab01aff64a90b935aa0f94a0cce62d10
Snayder Acero <helacer3@yahoo.es>
2021-09-05 20:26:01 -0500

    estructura base componentes maqueta la haus

---
+++ components/RealStates.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container-card">
+    <div class="slidecard"></div>
+    <div class="titlecard"></div>
+    <div class="sbtitlecard"></div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Header />
+    <RealStates />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  layout: 'LaHaus'
+})
 </script>
